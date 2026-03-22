06d578d10bc05805839b1eb9dd0e0c7dc8c4bceb
anuragpatwari591-hub <anuragpatwari591@gmail.com>
2026-03-22 15:06:39 +0530

    Improved output with startup message for better clarity

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Library System Running...");
         LibraryManager lib = new LibraryManager();
 
         // Add sample book
