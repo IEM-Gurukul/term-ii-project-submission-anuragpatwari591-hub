9db28dc1883b5343228cc6eeb452b69cc0c516b8
anuragpatwari591-hub <anuragpatwari591@gmail.com>
2026-03-23 18:59:04 +0530

    Added exception handling for invalid user input in menu system

--- src/Main.java
+++ src/Main.java
@@ -25,20 +25,20 @@ public class Main {
             try {
                 choice = sc.nextInt();
             } catch (Exception e) {
-                System.out.println("Invalid input! Enter number.");
-                sc.next();
+                System.out.println("Invalid input! Enter a number.");
+                sc.next(); // clear buffer
                 continue;
             }
 
+            // ---------------- ADD BOOK ----------------
             if (choice == 1) {
 
                 System.out.print("Enter Book ID: ");
                 int id = sc.nextInt();
 
-                if (id <= 0)
-                {
-                 System.out.println("Invalid ID");
-                 continue;
+                if (id <= 0) {
+                    System.out.println("Invalid ID");
+                    continue;
                 }
 
                 System.out.print("Enter Title: ");
@@ -51,10 +51,16 @@ public class Main {
                 lib.addBook(b);
             }
 
+            // ---------------- ADD USER ----------------
             else if (choice == 2) {
 
                 System.out.print("Enter User ID: ");
                 int id = sc.nextInt();
+
+                if (id <= 0) {
+                    System.out.println("Invalid ID");
+                    continue;
+                }
 
                 System.out.print("Enter Name: ");
                 String name = sc.next();
@@ -63,10 +69,13 @@ public class Main {
                 lib.addUser(u);
             }
 
+            // ---------------- VIEW BOOKS ----------------
             else if (choice == 3) {
+                System.out.println("\n--- Books List ---");
                 lib.viewBooks();
             }
 
+            // ---------------- ISSUE BOOK ----------------
             else if (choice == 4) {
 
                 System.out.print("Enter Book ID: ");
@@ -75,9 +84,15 @@ public class Main {
                 System.out.print("Enter User ID: ");
                 int uid = sc.nextInt();
 
+                if (bid <= 0 || uid <= 0) {
+                    System.out.println("Invalid IDs");
+                    continue;
+                }
+
                 lib.issueBook(bid, uid);
             }
 
+            // ---------------- EXIT ----------------
             else if (choice == 5) {
                 System.out.println("Exiting...");
                 break;
